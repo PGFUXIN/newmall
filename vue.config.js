8e65ec3de969822de0e1ef6039670c438e585fdd
PGFUXIN <765679737@qq.com>
2022-07-27 15:00:29 +0800

    提交css代码

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    resolve:{
+      extensions:['.js', '.json', '.jsx', '.css'],
+      alias:{
+        assets:"@/assets",
+        common:"@/common",
+        components:"@/components",
+        network:"@/network",
+        view:"@/view"
+      }
+    }
+  }
 }
